040c81b730d58dbfc844af25e171ffbe69b47e4e
chang-ha <kym637@naver.com>
2023-03-21 22:15:04 +0900

    우창하 : 2023-03-21숙제(수정)

--- CPlusPlus/HomeWork0321/HomeWork0321.cpp
+++ CPlusPlus/HomeWork0321/HomeWork0321.cpp
@@ -6,14 +6,14 @@
 
 struct Player
 {
-    int HP = 100;
-    int Att = 10;
+	int HP = 100;
+	int Att = 10;
 };
 
 struct Monster
 {
-    int HP = 100;
-    int Att = 10;
+	int HP = 100;
+	int Att = 20;
 };
 
 Player NewPlayer;
@@ -21,57 +21,55 @@ Monster NewMonster;
 
 void StatusRender(const char* const _Name, const int& _Att, const int& _HP)
 {
-    printf_s("----------- %s의 스테이터스 -----------\n", _Name);
-    printf_s("공격력 : %d\n", _Att);
-    printf_s("체력 : %d\n", _HP);
-    printf_s("-------------------------------------------\n");
+	printf_s("----------- %s의 스테이터스 -----------\n", _Name);
+	printf_s("공격력 : %d\n", _Att);
+	printf_s("체력 : %d\n", _HP);
+	printf_s("-------------------------------------------\n");
 }
 
-void Damage(const char* const _AttName,  // 공격자_이름 
-    const char* const _DefName,          // 방어자_이름
-    const int& _Attacker_Att,            // 공격자_공격력
-    int& _Defender_HP)                   // 방어자_체력
+void Damage(const char* const _Attacker_Name,  // 공격자_이름 
+	const char* const _Defender_Name,          // 방어자_이름
+	const int& _Attacker_Att,            // 공격자_공격력
+	int& _Defender_HP)                   // 방어자_체력
 {
-    _Defender_HP -= _Attacker_Att;
-    system("cls");
-    StatusRender("Player", NewPlayer.Att, NewPlayer.HP);
-    StatusRender("Monster", NewMonster.Att, NewMonster.HP);
-    printf_s("%s공격을 시작합니다\n", _AttName);
-    printf_s("%s가 %d의 데미지를 입었습니다\n", _DefName, _Attacker_Att);
+	_Defender_HP -= _Attacker_Att;
+	system("cls");
+	StatusRender("Player", NewPlayer.Att, NewPlayer.HP);
+	StatusRender("Monster", NewMonster.Att, NewMonster.HP);
+	printf_s("%s공격을 시작합니다\n", _Attacker_Name);
+	printf_s("%s가 %d의 데미지를 입었습니다\n", _Defender_Name, _Attacker_Att);
+	if (_Defender_HP == 0)
+	{
+		printf_s("%s가 죽었습니다.\n", _Defender_Name);
+		printf_s("%s의 승리입니다.\n", _Attacker_Name);
+	}
 }
 
 int main()
 {
-    StatusRender("Player", NewPlayer.Att, NewPlayer.HP);
-    _getch();
-    StatusRender("Monster", NewMonster.Att, NewMonster.HP);
-    _getch();
-    while (NewPlayer.HP && NewMonster.HP != 0)
-    {
-        system("cls");
-        switch (NewPlayer.HP == 0)
-        {
-        case true:
-            printf_s("플레이어가 죽었습니다.\n");
-            printf_s("몬스터의 승리입니다.\n");
-            _getch();
-            break;
-        default:
-            Damage("Player", "Monster", NewPlayer.Att, NewMonster.HP);
-            _getch();
-            break;
-        }
-        switch (NewMonster.HP == 0)
-        {
-        case true:
-            printf_s("몬스터가 죽었습니다.\n");
-            printf_s("플레이어의 승리입니다.\n");
-            _getch();
-            break;
-        default:
-            Damage("Monster", "Player", NewMonster.Att, NewPlayer.HP);
-            _getch();
-            break;
-        }
-    }
+	StatusRender("Player", NewPlayer.Att, NewPlayer.HP);
+	_getch();
+	StatusRender("Monster", NewMonster.Att, NewMonster.HP);
+	_getch();
+	while (NewPlayer.HP && NewMonster.HP != 0)
+	{
+		switch (NewPlayer.HP == 0)
+		{
+		case true:
+			break;
+		case false:
+			Damage("Player", "Monster", NewPlayer.Att, NewMonster.HP);
+			_getch();
+			break;
+		}
+		switch (NewMonster.HP == 0)
+		{
+		case true:
+			break;
+		case false:
+			Damage("Monster", "Player", NewMonster.Att, NewPlayer.HP);
+			_getch();
+			break;
+		}
+	}
 }
