94116c385973015513b24c3a97d1ed54164dab38
chang-ha <kym637@naver.com>
2023-04-05 22:41:02 +0900

    우창하 : 2023-04-05숙제

--- CPlusPlus/HomeWork0405/HomeWork0405.cpp
+++ CPlusPlus/HomeWork0405/HomeWork0405.cpp
@@ -7,7 +7,8 @@
 int main()
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-	GameEngineArray Array0(10);
+	int Array0Size = 10;
+	GameEngineArray Array0(Array0Size);
 	GameEngineArray Array1(5);
 
 	for (size_t i = 0; i < Array0.Count(); i++)
@@ -15,29 +16,29 @@ int main()
 		Array0[i] = i;
 	}
 
-	printf_s("----------Array0----------\n");
+	printf_s("----------Array0(%d)----------\n", Array0Size);
 	for (size_t i = 0; i < Array0.Count(); i++)
 	{
 		printf_s("%d\n", Array0[i]);
 	}
 
-	printf_s("----------LargeResize----------\n");
-
-	Array0.Resize(15);
+	Array0Size = 15;
+	printf_s("-------Array0 Resize(%d)-------\n", Array0Size);
+	Array0.Resize(Array0Size);
 	for (size_t i = 0; i < Array0.Count(); i++)
 	{
 		printf_s("%d\n", Array0[i]);
 	}
 
-	printf_s("----------SmallResize----------\n");
-
-	Array0.Resize(5);
+	Array0Size = 7;
+	printf_s("-------Array0 Resize(%d)-------\n", Array0Size);
+	Array0.Resize(Array0Size);
 	for (size_t i = 0; i < Array0.Count(); i++)
 	{
 		printf_s("%d\n", Array0[i]);
 	}
 	
-	printf_s("----------Array1 = Array0----------\n");
+	printf_s("--------Array1 = Array0--------\n");
 	Array1 = Array0;
 	for (size_t i = 0; i < Array1.Count(); i++)
 	{
